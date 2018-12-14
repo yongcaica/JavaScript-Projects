210cd46d124165c11b5ab4f6f9fff5f4d3229a24
yongcai <caiyong1@gmail.com>
2018-12-13 20:41:00 -0600

    update

--- common.js
+++
@@ -1,8 +0,0 @@
-/**
- * according to id, return the corresponding element
- * @param id is the id
- * @returns {HTMLElement} return element
- */
-function my$(id) {
-    return document.getElementById(id);
-}
